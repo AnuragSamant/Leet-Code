152ec4e50f3059beca929d13c5f73c389605c722
AnuragSamant <anuragsamant01@gmail.com>
2026-08-17 02:31:07 +0530

    Time: 105 ms (15.57%) | Memory: 9.4 MB (77.67%) - LeetSync

--- 1-two-sum/two-sum.c
+++ 1-two-sum/two-sum.c
@@ -1,7 +1,6 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
- //ReturnSize is number of matching case
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int *a=malloc(2*sizeof(int));
     for(int i=0;i<numsSize;i++){
